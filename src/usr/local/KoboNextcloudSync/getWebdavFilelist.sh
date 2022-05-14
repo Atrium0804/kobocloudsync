467c51289f6618ecb5fe61fae9087929ad714239
ivo.stieltjes <ivo.stieltjes@nslogin.nl>
2022-05-14 13:13:29 +0200

    Verplaatst, commentaar verduidelijkt

--- src/usr/local/KoboNextcloudSync/getWebdavFilelist.sh
+++ src/usr/local/KoboNextcloudSync/getWebdavFilelist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Download the file-list of a specified share
+# Download the file-list of a specified webdav-share
 # if the share is not password-protected, passwd is emtpy
 #
 # INPUT
@@ -9,18 +9,25 @@
 # passwd: the password of the share eg: ojrsAQ2MYQjNRxy
 #
 # Output: the relative path of the files on the server.
-# eg: /public.php/webdav/SharedFile.pdf
+# a recursive list of files is reported
+# eg: 
+# /public.php/webdav/SharedFile.pdf
+# /public.php/webdav/SubFolder/SharedFile2.pdf
+#
+# Limitations:
+# - works on shared folders only, not on shared files.
 
+
+# name the arguments
 davServer="$1"
 user="$2"
 passwd="$3"
 
 
 #load config
-# . $(dirname $0)/config.sh
-CURL=/usr/bin/curl
+. $(dirname $0)/config.sh
 
-
+# get the XML with files and extract the filepaths
 echo '<?xml version="1.0"?>
 <a:propfind xmlns:a="DAV:">
 <a:prop><a:resourcetype/></a:prop>
