19bfe2d7b958cd88c8a17a1a3f0dfde9dedb115e
ivo.stieltjes <ivo.stieltjes@nslogin.nl>
2022-05-18 22:38:00 +0200

    bugfix

--- src/usr/local/KoboCloudSync/config.sh
+++ src/usr/local/KoboCloudSync/config.sh
@@ -8,10 +8,19 @@ KC_HOME=$(dirname $0)
 ConfigTemplate=$KC_HOME/KoboCloudSync.conf.tmpl
 
 
-if uname -a | grep -q 'x86\|Darwin'
+if uname -a | grep -q 'Darwin.*ARM64'  # mac M1
 then
+    #echo "Mac M1 detected"
+    kepubify=kepubify-darwin-arm64
+    seriesmeta=seriesmeta-linux-arm
+    covergen=covergen-darwin-arm64
+    . $KC_HOME/config_dev.sh
+elif uname -a | grep -q 'Darwin.*X86_64'  # mac intel
+    kepubify=kepubify-darwin-arm
+    seriesmeta=seriesmeta-darwin-arm
+    covergen=covergen-darwin-arm
     #echo "PC detected"
-    . $KC_HOME/config_pc.sh
+    . $KC_HOME/config_dev.sh
 else
     . $KC_HOME/config_kobo.sh
 fi
