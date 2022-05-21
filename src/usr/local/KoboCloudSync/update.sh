51246ac30e890f10bf69500d165dc87216d83b5d
Ivo Stieltjes <ivo.stieltjes@nslogin.nl>
2022-05-21 09:25:52 +0200

    updated update.sh

---
+++ src/usr/local/KoboCloudSync/update.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Download the KoboRoot.tgz-file from GitHub and install on kobo
+
+theGitHubURL="https://github.com/Atrium0804/KoboNextcloudsync/raw/main/KoboRoot.tgz"
+theArchive="/tmp/KoboRoot.tgz"
+
+if uname -a | grep -q 'Darwin'
+then
+    #echo "MacOS detected"
+    theArchive="/tmp/KoboRoot.tgz"
+	theExtractFolder="/tmp/kobocloudsync"
+else
+    theArchive="/tmp/KoboRoot.tgz"
+	theExtractFolder="/"
+fi
+
+
+# download KoboRoot.tgz from GitHub
+[ ! -e "$theExtractFolder" ] && mkdir -p "$theExtractFolder" >/dev/null 2>&1
+wget $theGitHubURL -O $theArchive
+
+# install: extract to root
+if tar -zxvf $theArchive --directory $theExtractFolder ; 
+	then rm -f $theArchive
+fi
+# start udev_mount to create required foldres
+. $theExtractFolder/usr/local/kobocloudsync/udev_mount.sh
