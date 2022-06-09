340fd8a11ddf1b487c04cd74f52d4c01eb86ba63
ivo.stieltjes <ivo.stieltjes@nslogin.nl>
2022-06-09 09:20:03 +0200

    rename to main.sh

--- src/usr/local/KoboCloudSync/main.sh
+++ src/usr/local/KoboCloudSync/main.sh
@@ -1,86 +1,47 @@
 #!/bin/sh
 
-TEST=$1
-
-# v2 cut ipv <<<
-
 #load config
 . $(dirname $0)/config.sh
-echo "${cyan}################## Main ##################${NC}"
-# Read the configfile and 
-# check if Kobocloud contains the line "UNINSTALL"
-if grep -q '^UNINSTALL$' $UserConfig; then
-    echo "Uninstalling KoboCloud!"
-    $KC_HOME/uninstall.sh
-    exit 0
+
+
+echo "`$Dt` start" 
+
+# check working network connection
+$KC_HOME/checkNetwork.sh
+hasNetwork=$?
+if [ $hasNetwork -ne 0 ]; 
+then 
+    echo "$RED No network connection, aborting"
+    exit 1
 fi
 
-# Starting new filelist
-# add filelist itself to prevent from pruning
-echo "$RemoteFileList" > "$RemoteFileList"
+#  get remote shares and download files
+echo "$CYAN get shares $NC"
+shares=`$rclone listremotes $rcloneOptions | sed 's/://' `
+echo "$shares" |
+while IFS= read -r currentShare; do
+    ./downloadFiles.sh "$currentShare"
+done
 
-# test if the an internet-connection is available
-# by pinging aws.amazon.com
-case $device in
-  "kobo") waitparm='-w' ;;
-       *) waitparm='-i' ;;
-esac
-
-if [ "$TEST" = "" ]
-then
-    echo "`$Dt` waiting for internet connection"
-    eval "$fbink \"waiting for internet connection\" "
-    r=1;i=0
-    while [ $r != 0 ]; do
-    if [ $i -gt 60 ]; then
-        ping -c 1 $waitparm 3 aws.amazon.com #>/dev/null 2>&1
-        echo "`$Dt` error! no connection detected" 
-        "$fbink  \"error! no connection detected\" " 
-        exit 1
-    fi
-    ping -c 1 $waitparm 3 aws.amazon.com #>/dev/null 2>&1
-    r=$? # get the exit-status of the previous cmd, 0=successful
-    if [ $r != 0 ]; then sleep 1; fi
-    i=$(($i + 1))
-    done
+# check network again as the kobo might close the wifi after a while
+# check working network connection
+echo "$CYAN Pruning folders $NC"
+$KC_HOME/checkNetwork.sh
+hasNetwork=$?
+if [ $hasNetwork -ne 0 ]; 
+then 
+    echo "$RED No network connection, aborting"
+    exit 1
 fi
-
-
-# process the config-file
-IFS=',' #setting comma as delimiter  
-while read line || [ -n "$line" ]; do
-#   echo "Reading $line"
-  if echo "$line" | grep -q '^#'; then
-   exec # comment found, do nothing
-  elif [ "$line" = "" ]; then
-    exec # empty line
-  elif echo "$line" | grep -q "^REMOVE_DELETED$"; then
-	  echo "Files deleted on the server will be removed from this device."
-  else
-    echo "$YELLOW processing: $line $NC"
-    # split the line in DestinationFolder, URL and password
- 	  destFolder=$(echo "$line" | cut -d, -f1)
-	  url=$(echo "$line" | cut -d, -f2)
-	  pwd=$(echo "$line" | cut -d, -f3-)   
-    pwd=$(echo "$pwd" | tr -d [:blank:]) # trim spacees
-    
-    # echo "-${pwd}-"
-    destFolderAbsolute="$DocumentRoot/$destFolder"
-    eval "$fbink \"Processing $destFolder\" " 
-    echo  "Processing $destFolder"
-    $KC_HOME/getNextcloudFiles.sh "$url" "$destFolderAbsolute" "$pwd"
-    if [ -n "$destFolder" ]; then 
-      # only prune when destFolder is not null, otherwise we are deleting too much...
-
-    eval "$fbink \"Removing deleted files\" " 
-    echo "Removing deleted files" 
-      $KC_HOME/pruneFolder.sh "$destFolderAbsolute"
-    fi
-  fi
-done < $UserConfig
+$KC_HOME/pruneFolders.sh
 
 # generate covers
-eval "$fbink \"Generating Covers\" " 
-echo "Generating Covers" 
-$covergen -g $DocumentRoot >/dev/null 2>&1
-eval "$fbink \"KoboCloudSync finished\" " 
+$covergen -g $DocumentRoot
+
+
+# rclone sync   - Make source and dest identical, modifying destination only.
+# rclone ls     - List all the objects in the path with size and path.
+# rclone rmdirs - Remove any empty directories under the path.
+
+# ./rclone ls test:/ --config=rclone.config 
+# ./rclone sync  test:/ ./data --config=rclone.config 
